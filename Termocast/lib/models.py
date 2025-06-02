300ce400a77ca242872e8806011fcbf9ea914267
Hildah-art <njagahildah@gmail.com>
2025-06-02 10:42:50 +0300

    Created the weather._db database

---
+++ Termocast/lib/models.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+
+Base = declarative_base()
